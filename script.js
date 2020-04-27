04722da8a47e8253783164603bfd6a67218e94ea
Korsso <korsso74@gmail.com>
2020-04-27 14:48:19 +0300

    Урок 8.2

--- script.js
+++ script.js
@@ -3,6 +3,35 @@
 //} else {
   //  console.log("Неверно")
 //}
+<<<<<<< HEAD
+//let num = 50;
+
+//if (num < 49) {
+//    console.log('Неверно!')
+//} else if(num > 100) {
+  //  console.log("Много") 
+//} else {
+  //  console.log('Верно!')
+//}; 
+//num == 50 ? console.log('Верно!') : console.log('Неверно!');
+
+//switch (num) {
+  //  case num < 49:
+    //    console.log('Неверно!');
+      //  break;
+    //case num > 100:
+      //  console.log('Много');
+        //break;
+    //case num > 80:
+    //    console.log('Eще Много');
+      //  break;
+    //case 50:
+     //   console.log('Верно!');
+    //    break;
+    //default:
+    //    console.log('Что то пошло не так');  
+    //    break;
+=======
 let num = 50;
 
 if (num < 49) {
@@ -30,5 +59,6 @@ switch (num) {
     default:
         console.log('Что то пошло не так');  
         break;
+>>>>>>> 760e5e54ac9b7c59784737910f0a7ad3424ac1f1
 
 }     
